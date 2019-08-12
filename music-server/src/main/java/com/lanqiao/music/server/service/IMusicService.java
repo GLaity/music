19083fb7daf38bb045f9c840cf018df136f86591
root <root@lanqiao.org>
2019-08-12 14:03:57 +0800

    后台页面

--- music-server/src/main/java/com/lanqiao/music/server/service/IMusicService.java
+++ music-server/src/main/java/com/lanqiao/music/server/service/IMusicService.java
@@ -1,5 +1,10 @@
 package com.lanqiao.music.server.service;
 
+import com.lanqiao.music.server.pojo.Music;
+
+import java.util.List;
+
 public interface IMusicService {
 
+    List<Music> getAllMusic();
 }
