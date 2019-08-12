b281dd8ce3a3fc389b293e2adb71b8dcbc3b4d3d
ljc <ljc@lanqiao.org>
2019-08-12 18:11:06 +0800

    后台用户

--- music-server/src/test/java/com/lanqiao/music/server/MusicServerApplicationTests.java
+++ music-server/src/test/java/com/lanqiao/music/server/MusicServerApplicationTests.java
@@ -1,16 +1,35 @@
 package com.lanqiao.music.server;
 
+import com.lanqiao.music.server.dao.SheetMapper;
+import com.lanqiao.music.server.dao.UserMapper;
+import com.lanqiao.music.server.pojo.Sheet;
+import com.lanqiao.music.server.pojo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.SQLException;
+import java.util.List;
+
+@MapperScan("com.lanqiao.music.server.dao")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MusicServerApplicationTests {
+    @Autowired
+    private SheetMapper sheetMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws SQLException {
+//        System.out.println("-----------");
+//        List<Sheet> sheets =sheetMapper.searchAll();
+//        System.out.println(sheets);
+        User user = userMapper.selectByPrimaryKey(1);
+        System.out.println(user);
 
     }
 
