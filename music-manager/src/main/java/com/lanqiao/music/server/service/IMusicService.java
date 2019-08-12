a1a8cc4c2cd45a004ca93b24a02fc90131497cf2
sw <sw@lanqiao.org>
2019-08-12 17:54:07 +0800

    Merge branch 'zyp' of https://github.com/GLaity/music
    # Conflicts:
    #	music-front/src/main/java/com/lanqiao/music/server/service/ISheetService.java
    #	music-front/src/main/resources/application.yaml
    #	music-server/src/main/java/com/lanqiao/music/server/dao/AdviceMapper.java
    #	music-server/src/main/java/com/lanqiao/music/server/dao/SheetMapper.java
    #	music-server/src/main/java/com/lanqiao/music/server/dao/impl/AdviceMapper.xml
    #	music-server/src/main/java/com/lanqiao/music/server/dao/impl/SheetMapper.xml
    #	music-server/src/main/java/com/lanqiao/music/server/dao/impl/UserMapper.xml
    #	music-server/src/main/java/com/lanqiao/music/server/service/ISheetService.java
    #	music-server/src/main/java/com/lanqiao/music/server/service/impl/SheetService.java
    #	music-server/src/main/resources/application.yaml
    #	music-server/src/test/java/com/lanqiao/music/server/MusicServerApplicationTests.java

--- music-manager/src/main/java/com/lanqiao/music/server/service/IMusicService.java
+++ music-manager/src/main/java/com/lanqiao/music/server/service/IMusicService.java
@@ -1,7 +1,13 @@
 package com.lanqiao.music.server.service;
+
+import com.lanqiao.music.server.pojo.Music;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface IMusicService {
+    List<Music> getAllMusic();
 
+    void addMusic(Music music);
 }
