c29248ebc0a21bc0e01a62c9a63d8a250ed74c97
gwz <874664974@qq.com>
2019-08-17 09:42:13 +0800

    静态资源

--- music-front/src/main/java/com/lanqiao/music/front/controller/MusicController.java
+++ music-front/src/main/java/com/lanqiao/music/front/controller/MusicController.java
@@ -14,8 +14,8 @@ public class MusicController {
     @Reference
     private IMusicService iMusicService;
 
-    @Reference
-    private ISheetService iSheetService;
+//    @Reference
+//    private ISheetService iSheetService;
     @RequestMapping("/music")
     public String findAllMusic(Model model){
         List<Music> musicList = iMusicService.queryAllMusic();
