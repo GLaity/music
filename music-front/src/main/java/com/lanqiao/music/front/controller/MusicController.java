975a3343923fd71fad93d4414e004de1d8ca6d09
gwz <874664974@qq.com>
2019-08-18 16:23:32 +0800

    播放歌曲以及添加列表

--- music-front/src/main/java/com/lanqiao/music/front/controller/MusicController.java
+++ music-front/src/main/java/com/lanqiao/music/front/controller/MusicController.java
@@ -2,11 +2,16 @@ package com.lanqiao.music.front.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.lanqiao.music.server.pojo.Music;
+import com.lanqiao.music.server.pojo.MusicJson;
 import com.lanqiao.music.server.service.IMusicService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
+import java.util.LinkedList;
 import java.util.List;
 
 @Controller
@@ -36,4 +41,39 @@ public class MusicController {
 //        model.addAttribute("userSheet",userSheet);
 //        return "index";
 //    }
+
+    @RequestMapping("/getMusic")
+    @ResponseBody
+    public List<MusicJson> getMusic(HttpSession session){
+        List<MusicJson> musicJsons = (List<MusicJson>) session.getAttribute("musicJsons");
+        if (musicJsons == null) {
+            MusicJson musicJson = new MusicJson();
+            musicJson.setTitle("儿歌");
+            musicJson.setArtist("老王");
+            musicJson.setMp3("./content/songs/儿歌.mp3");
+            musicJson.setPoster("./content/songs/儿歌.jpg");
+
+            musicJsons = new LinkedList<>();
+            musicJsons.add(musicJson);
+            session.setAttribute("musicJsons",musicJsons);
+        }
+        return musicJsons;
+    }
+    @RequestMapping("/playMusic/{id}")
+    @ResponseBody
+    public List<MusicJson> playMusic(HttpSession session, @PathVariable Integer id){
+        Music music = iMusicService.queryMusicByMId(id);
+        MusicJson musicJson = new MusicJson();
+        musicJson.setTitle(music.getMname());
+        musicJson.setArtist(music.getMsinger());
+        musicJson.setMp3(music.getMloc());
+        musicJson.setPoster("");
+        List<MusicJson> musicJsons = (List<MusicJson>) session.getAttribute("musicJsons");
+        if (musicJsons != null) {
+            //添加元素到栈顶
+            musicJsons.add(0,musicJson);
+        }
+        session.setAttribute("musicJsonList",musicJsons);
+        return musicJsons;
+    }
 }
